8619878e1e32491a1f3528d5ec6a90308edb1f96
Eric <guillaumeric@icloud.com>
2020-03-23 18:20:03 +0100

    congervence: input noise

--- remote_run_impl.sh
+++ remote_run_impl.sh
@@ -4,6 +4,7 @@ rm -rf keras-dcgan
 rm -rf /tmp/logs
 git clone https://github.com/ericguillaume/keras-dcgan.git
 cd keras-dcgan
+git checkout resize_images_mnist
 mkdir -p eric_generated
 mkdir -p generated/model
 python3 eric.py
